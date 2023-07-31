761e3ca61d662c6ebedc735c25346d3a4c43dd48
Danil <baba_ejik@mail.ru>
2023-07-31 15:33:55 +0600

    43 lesson

--- src/components/Dialogs/DialogsName/DialogsName.tsx
+++ src/components/Dialogs/DialogsName/DialogsName.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import s from './../Dialogs.module.css'
 import {NavLink} from "react-router-dom";
-import {DialogsDataType, StateType} from "../../Redux/State";
+import {DialogsDataType} from "../../Redux/reducers/messageReducer";
 type NameUsersPropsType = {
-    stateMessageUser: StateType
+    DialogsData: DialogsDataType[]
 }
 export const NameUsers: React.FC<NameUsersPropsType> = (props) => {
-    let dialogsElement = props.stateMessageUser.MessagePage.DialogsData.map(el => <DialogItem key={el.id} name={el.name} id={el.id}/>)
+    let dialogsElement = props.DialogsData.map(el => <DialogItem key={el.id} name={el.name} id={el.id}/>)
     return (
         <div className={s.name_users}>{dialogsElement}</div>
     )
